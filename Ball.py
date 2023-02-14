789932b8faff4d3363a0cfe9b0c6e71563559705
GustavoAntony <gustavoa9@al.insper.edu.br>
2023-02-14 16:12:36 -0300

    size

--- Ball.py
+++ Ball.py
@@ -3,6 +3,6 @@ import pygame
 class Ball(pygame.sprite.Sprite):
     def __init__(self):
         super(Ball, self).__init__()
-        self.surf = pygame.Surface((75, 25))
+        self.surf = pygame.Surface((50,50))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
