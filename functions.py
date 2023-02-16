3f4ada1dd479b22cac758b0179eedb03bdc71cc4
GustavoAntony <gustavoa9@al.insper.edu.br>
2023-02-16 15:16:38 -0300

    gustavo

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import pygame
 from Ball import Ball
+from Button import Button
 
 HEIGHT = 900
 WIDTH = 700
@@ -8,16 +9,56 @@ screen= pygame.display.set_mode((WIDTH,HEIGHT))
 
 #TELA INICIAL
 def inicial_screen(running, window):
-    window = "inicial"
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            window = "nivel_1"
-        elif event.type == pygame.QUIT:
-            running = False
-    
-        screen.fill((0,0,0))
+    window = 'inicial'
+    import pygame
+    import sys
+
+    pygame.init()
+
+    screen_width = 700
+    screen_height = 900
+    background_color = (255, 255, 255)
+
+    screen = pygame.display.set_mode((screen_width, screen_height))
+
+    start_button_width = 200
+    start_button_height = 50
+    start_button_x = (screen_width - start_button_width) // 2
+    start_button_y = screen_height // 2 - 50
+    start_button = pygame.Rect(start_button_x, start_button_y, start_button_width, start_button_height)
+
+    exit_button_width = 200
+    exit_button_height = 50
+    exit_button_x = (screen_width - exit_button_width) // 2
+    exit_button_y = screen_height // 2 +25
+    exit_button = pygame.Rect(exit_button_x, exit_button_y, exit_button_width, exit_button_height)
+
+    font = pygame.font.Font(None, 36)
+    start_text = font.render("Start", True, (0, 0, 0))
+    exit_text = font.render("Sair", True, (0, 0, 0))
+
+    while window == "inicial":
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN and start_button.collidepoint(event.pos):
+                print("Botão Start clicado!")
+                window = "nivel_1"
+
+            if event.type == pygame.MOUSEBUTTONDOWN and exit_button.collidepoint(event.pos):
+                pygame.quit()
+                sys.exit()
+
+        screen.fill(background_color)
+        pygame.draw.rect(screen, (255, 0, 0), start_button) # desenha o botão "Start"
+        pygame.draw.rect(screen, (255, 0, 0), exit_button) # desenha o botão "Sair"
+        screen.blit(start_text, (start_button.x + 70, start_button.y + 15)) # desenha o texto "Start"
+        screen.blit(exit_text, (exit_button.x + 80, exit_button.y + 15)) # desenha o texto "Sair"
         pygame.display.update()
-    return running, window
+
+    return True, window
     
     
 def nivel_1(running, window):
